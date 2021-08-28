b1e6a688c9cb68742fe3a4b556ee46a5d7463e18
Narvaez0993 <61300610+Narvaez0993@users.noreply.github.com>
2021-08-28 17:23:05 -0500

    creando objetos

---
+++ objetos.js
@@ -0,0 +1,22 @@
+let estudiant = {
+    
+    nombre: "juan",
+    edad:32,
+    carrera: "Desarrollo de software",
+    genero : "M",
+    telefono : "322562363",
+    estatura : 1.60,
+    hinchadelmejor: true,
+    materias: ["calculo" , "fisica", "logica"],
+    comidas:{
+        
+        postre: "tiramizu",
+        fuerte: "frijolitos",
+        bebida: "guara"
+    } 
+
+
+}
+
+
+console.log(estudiant.nombre)
